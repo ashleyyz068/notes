8bffd2e261864fa6fcb2a8f85006da15ec425d25
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-01-08 16:38:42 -0500

    d

--- app.js
+++ app.js
@@ -28,43 +28,55 @@
 
 // getData(URL);
 
-const months = ['Jan', 'March', 'April', 'June'];
-months.splice(1, 0, 'Feb');
-// Inserts at index 1
-console.log(months);
-// Expected output: Array ["Jan", "Feb", "March", "April", "June"]
+// const months = ['Jan', 'March', 'April', 'June'];
+// months.splice(1, 0, 'Feb');
+// // Inserts at index 1
+// console.log(months);
+// // Expected output: Array ["Jan", "Feb", "March", "April", "June"]
 
-const dennis = {
-    name: "dennis",
-    age: 5,
-    color: "blue",
-};
+// const dennis = {
+//     name: "dennis",
+//     age: 5,
+//     color: "blue",
+// };
 
-function age (d){
-    return d.age
-};
+// function age (d){
+//     return d.age
+// };
 
-console.log(age(dennis));
+// console.log(age(dennis));
 
-console.log(Array.from(dennis.name)); 
-x = [1,2,3,4]; 
-y=[1,2,3,4,5];
-z=[...x,...y];
-console.log(z)
+// console.log(Array.from(dennis.name)); 
+// x = [1,2,3,4]; 
+// y=[1,2,3,4,5];
+// z=[...x,...y];
+// console.log(z)
 
-if (dennis.name.includes("den")){
-    console.log("true"); 
-} else {
-    console.log("false")
-}
+// if (dennis.name.includes("den")){
+//     console.log("true"); 
+// } else {
+//     console.log("false")
+// }
 
-sum = 0;
-const data = [1,2,3,4,5]
-for (i=0; i<data.length; i++){
-    sum+=data[i]
-}
-console.log(sum)
+// sum = 0;
+// const data = [1,2,3,4,5]
+// for (i=0; i<data.length; i++){
+//     sum+=data[i]
+// }
+// console.log(sum)
 
 // while (){
 
 // }
+
+let findDiff = function (s, t) {
+// sequencing starts here sorting 
+    let sortS = [...s].sort();
+    let sortT = [...t].sort();
+    for (let i = 0; i < sortT.length; i++) {
+      if (sortT[i] != sortS[i]) {
+        return sortT[i];
+      }
+    }
+  };
+  console.log(findDiff("abdcef", "abcduef"));
